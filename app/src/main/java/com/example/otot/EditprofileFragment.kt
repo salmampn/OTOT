fd672e201b02a203fb74836f71652b52432b91c0
RizkyAliZamzami <izamrizkyali@gmail.com>
2024-10-25 19:11:34 +0700

    Build Post Running Fragment and Edit Profile

--- app/src/main/java/com/example/otot/EditprofileFragment.kt
+++ app/src/main/java/com/example/otot/EditprofileFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -60,8 +61,8 @@ class EditprofileFragment : Fragment() {
                 etUsername.text.toString(),
                 spinnerGender.selectedItem.toString()
             )
+            findNavController().navigate(R.id.action_editprofileFragment_to_profileFragment)
         }
-
         return view
     }
 
